b65a338b62e0356212981782ad8d831cc826ecc0
Cracktos <falzarano.alejandro@gmail.com>
2017-10-23 01:04:35 -0300

    Polished base version

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,39 +2,43 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-	public float displacementForce;
+    public float displacementForce;
+    public float rotationSpeed;
 
-	public float rotationSpeed;
+    private Rigidbody _rigidBody;
 
-	private Rigidbody _rigidBody;
+    void Start()
+    {
+        _rigidBody = GetComponent<Rigidbody>();
+    }
 
-	void Start()
-	{
-		_rigidBody = GetComponent<Rigidbody>();
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+            _rigidBody.AddForce(transform.forward * displacementForce);
 
-	}
+        if (Input.GetKey(KeyCode.A))
+            _rigidBody.AddForce(-transform.right * displacementForce);
 
-	void FixedUpdate()
-	{
-		if (Input.GetKey(KeyCode.W))
-			_rigidBody.AddForce(transform.forward * displacementForce);
+        if (Input.GetKey(KeyCode.S))
+            _rigidBody.AddForce(-transform.forward * displacementForce);
 
-		if (Input.GetKey(KeyCode.S))
-			_rigidBody.AddForce(-transform.forward * displacementForce);
+        if (Input.GetKey(KeyCode.D))
+            _rigidBody.AddForce(transform.right * displacementForce);
 
-		Plane _plane = new Plane(Vector3.up, transform.position);
+        Plane _plane = new Plane(Vector3.up, transform.position);
 
-		Ray _ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Ray _ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-		float hitDistance = default(float);
+        float hitDistance = default(float);
 
-		if (_plane.Raycast(_ray, out hitDistance))
-		{
-			Vector3 targetPoint = _ray.GetPoint(hitDistance);
+        if (_plane.Raycast(_ray, out hitDistance))
+        {
+            Vector3 targetPoint = _ray.GetPoint(hitDistance);
 
-			Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+            Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
 
-			transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
-		}
-	}
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        }
+    }
 }
